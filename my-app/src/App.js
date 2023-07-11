f29e555b9002bcc3846c85fcadbe9b5fe930ec19
aarif2000 <kaarif960@gmail.com>
2023-07-11 23:02:03 +0530

    added router

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,14 +2,58 @@ import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import About from './components/About';
+import Alert from './components/Alert'
+import React, {useState} from 'react'
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
+  const [mode, setMode] = useState('light');
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type) =>{
+    setAlert({
+      msg: message,
+      type: type
+    })
+    setTimeout(() => {
+      setAlert(null);
+    }, 2000);
+  }
+  const toggleMode = () =>{
+    if(mode === 'light'){
+      setMode('dark');
+      document.body.style.backgroundColor = '#3a373b'
+      showAlert("Dark mode has been enabled", "success")
+    }
+    else {
+      setMode('light');
+      document.body.style.backgroundColor = 'white'
+      showAlert("Light mode has been enabled", "success")
+
+    }
+  }
   return (
   <>
-    <Navbar/>
-    <div className= "container">
-    <About/>
+  <Router>
+    <Navbar mode={mode} toggleMode= {toggleMode} />
+    <Alert alert= {alert}/>
+    <div className= "container my-2">
+    <Routes>
+          <Route exact path="/about" element = { <About/>}>
+          </Route>
+          <Route exact path="/" element = {<TextForm/>}>
+          </Route>
+          <Route exact path="/home" element = {<TextForm/>}>
+          </Route>
+    </Routes>
     </div>
+  </Router>
   </>
   );
 }
--- my-app/src/components/TextForm.js
+++ my-app/src/components/TextForm.js
@@ -4,16 +4,21 @@ export default function TextForm(props) {
   const HandleupClick = () =>{
     let newText = text.toUpperCase()
     setText(newText); 
+    props.showAlert("Changed to UpperCase", "success")
 }
 
 const HandleclearClick = () =>{
   let newText = text.toLowerCase()
   setText(' '); 
+  props.showAlert("Text Cleared", "success")
+
 }
 
 const HandledownClick = () =>{
   let newText = text.toLowerCase()
   setText(newText); 
+  props.showAlert("Changed to LowerCase", "success")
+
 }
   const handleOnChange = (event) =>{
     console.log("Done")
@@ -21,21 +26,21 @@ const HandledownClick = () =>{
   }
   return (
     <>
-  <div class="mb-3">
+  <div class="container" style={{Color: props.mode === 'dark'? 'white' : 'black'}}>
     <h1>{props.heading}</h1>
     <label htmlFor="exampleFormControlTextarea1" className="form-label"></label>
-    <textarea className="form-control" value={text} onChange={handleOnChange} id="exampleFormControlTextarea1" rows="8"></textarea>
-    <button className="btn btn-primary mx-2" onClick={HandledownClick}>Convert to Lowercase </button>
+    <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode === 'dark'? 'grey' : 'light'}} id="exampleFormControlTextarea1" rows="8"></textarea>
+    <button className="btn btn-primary mx-2 my-3" onClick={HandledownClick}>Convert to Lowercase </button>
     <button className="btn btn-primary" onClick={HandleupClick}>Convert to Uppercase</button>
-    <button className="btn btn-primary mx-2" onClick={HandleclearClick}>Clear Text</button>
+    <button className="btn btn-primary mx-2 my-3" onClick={HandleclearClick}>Clear Text</button>
 
   </div>
 
-    <div class="container my-3">
+    <div class="container my-3" style={{Color: props.mode === 'dark'? 'white' : 'black'}}>
      <h1> Your Summary here</h1>
       Words are {text.split(" ").length} and characters are {text.length}
       <h2>Preview</h2>.
-      <p>{text}</p>
+      <p>{text.length>0? text : 'Enter text above to preview '}</p>
     </div>
     </>
   )
